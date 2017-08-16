65e624939693085c73f67185b114dc5357e6804b
Jerry Liu <3jerryliu@gmail.com>
2017-08-16 02:30:23 -0400

    Add files via upload

--- DataAnalytics.m
+++ DataAnalytics.m
@@ -1,9 +1,21 @@
 c = 2.998e8;
-radii = 350:5:350;
-heights = 1000:100:1000;
+radii = 350:5:500; %36 points
+heights = 500:100:1500; %11 points
+% for height = 1:11
+% figure;
+% %imagesc(wavelengths (100), radius (401), phasex (100x401) transpose)
+% imagesc(radius*2, heights(height), mod(unwrapped_phasex(:, :, height), 2*pi)');
+% colorbar;
+% figure;
+% imagesc(radii, heights(height), pillar_reflection(:, :, height)');
+% colorbar;
+% end
+
+variable1 = squeeze(unwrapped_phasex(1,:,:));
+variable2 = squeeze(pillar_reflection(1, :, :));
 figure;
-imagesc(radius*2, heights, mod(upx, 2*pi)');
+imagesc(radii, heights, mod(variable1, 2*pi)');
 colorbar;
 figure;
-imagesc(radius*2, heights, t');
+imagesc(radii, heights, variable2');
 colorbar;
